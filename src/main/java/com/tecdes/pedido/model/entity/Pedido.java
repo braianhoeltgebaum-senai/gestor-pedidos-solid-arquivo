5074de1dea0fd7114313d9dcde8dee84cd03fc44
Djúlia Sabrina Preti <djulia_preti@estudante.sesisenai.org.br>
2025-11-06 19:02:28 -0300

    Pedido e Produto Entity - Leonardo
    Código do pedido e produto da pasta entity.

--- src/main/java/com/tecdes/pedido/model/entity/Pedido.java
+++ src/main/java/com/tecdes/pedido/model/entity/Pedido.java
@@ -1,5 +1,95 @@
 package com.tecdes.pedido.model.entity;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pedido {
+    private int id;
+    private LocalDateTime dataHora;
+    private String status;
+    private List<ItemPedido> produtos;
+    private double valorTotal;
+    private String TipoPagamento;
+    @SuppressWarnings("rawtypes")
+    private ArrayList itens;
+    
+        public void pedido() {
+            this.dataHora = LocalDateTime.now();
+            this.status = "Recebido";
+            this.itens = new ArrayList<>();
+            this.valorTotal = 0.0;
+
+    }
+
+    public void calcularTotal () {
+        double total = 0.0;
+        for (Object item : itens) {
+            if (item instanceof ItemPedido) {
+                total += ((ItemPedido) item).calcularTotal();
+            }
+        }
+        
+        this.valorTotal = total;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<ItemPedido> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<ItemPedido> produtos) {
+        this.produtos = produtos;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getTipoPagamento() {
+        return TipoPagamento;
+    }
+
+    public void setTipoPagamento(String tipoPagamento) {
+        TipoPagamento = tipoPagamento;
+    }
+
+    @SuppressWarnings("rawtypes")
+    public ArrayList getItens() {
+        return itens;
+    }
+
+    public void setItens(@SuppressWarnings("rawtypes") ArrayList itens) {
+        this.itens = itens;
+    }
+
+
 
 }
--- src/main/java/com/tecdes/pedido/model/entity/Produto.java
+++ src/main/java/com/tecdes/pedido/model/entity/Produto.java
@@ -2,4 +2,32 @@ package com.tecdes.pedido.model.entity;
 
 public class Produto {
 
+    int idProduto;
+    String nome;
+    double preco;
+
+    
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+    public int getIdProduto() {
+        return idProduto;
+    }
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
 }
