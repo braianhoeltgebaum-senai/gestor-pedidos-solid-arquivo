b9ebd8f7730ab647c4a1ddc7444a72862bd0c778
Djúlia Sabrina Preti <djulia_preti@estudante.sesisenai.org.br>
2025-11-25 19:38:06 -0300

    Entrega - Dju
    Atualização no código da entrega.

--- src/main/java/com/tecdes/pedido/model/entity/Entrega.java
+++ src/main/java/com/tecdes/pedido/model/entity/Entrega.java
@@ -1,18 +1,98 @@
 package com.tecdes.pedido.model.entity;
 
+import java.time.LocalDate;
+
 public class Entrega {
 
-    private int id;
-    private String endereco;
-    private String status;
+   
+    private Long idEntrega;
+    private String enderecoEntrega; 
+    private String status; 
+    private LocalDate dataPrevista;
+    private LocalDate dataEntrega;
+    
 
-    public Entrega(int id, String endereco) {
-        this.id = id;
-        this.endereco = endereco;
-        this.status = "Pendente";
+    
+    private Pedido pedido; 
+
+    
+    public Entrega() {
+        this.status = "Preparando";
+        
+    }
+    
+    
+    public Entrega(String enderecoEntrega, LocalDate dataPrevista, Pedido pedido) {
+        this(); 
+        setEnderecoEntrega(enderecoEntrega);
+        setDataPrevista(dataPrevista);
+        setPedido(pedido);
     }
 
-    public void atualizarStatus(String status){
+   
+    
+    /**
+     * Atualiza o status da entrega.
+     * @param novoStatus O novo status da entrega.
+     */
+    public void atualizarStatus(String novoStatus) {
+        this.status = novoStatus;
+        
+        
+        if ("Entregue".equalsIgnoreCase(novoStatus)) {
+            this.dataEntrega = LocalDate.now();
+        }
+        System.out.println("Entrega " + idEntrega + " - Novo Status: " + novoStatus);
+       
+    }
+
+  
+
+    public Long getIdEntrega() {
+        return idEntrega;
+    }
+
+    public void setIdEntrega(Long idEntrega) {
+        this.idEntrega = idEntrega;
+    }
+
+    public String getEnderecoEntrega() {
+        return enderecoEntrega;
+    }
+
+    public void setEnderecoEntrega(String enderecoEntrega) {
+        this.enderecoEntrega = enderecoEntrega;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
         this.status = status;
+    }
+
+    public LocalDate getDataPrevista() {
+        return dataPrevista;
+    }
+
+    public void setDataPrevista(LocalDate dataPrevista) {
+        this.dataPrevista = dataPrevista;
+    }
+
+    public LocalDate getDataEntrega() {
+        return dataEntrega;
+    }
+
+    public void setDataEntrega(LocalDate dataEntrega) {
+        this.dataEntrega = dataEntrega;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
     }
 }
